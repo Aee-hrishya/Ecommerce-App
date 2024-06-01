213e3cf64501a4fe64cfc1303e943532eda30b42
hrishikesh <hshinde166@gmail.com>
2024-06-02 00:44:31 +0530

    created an array for different categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import Card from "./Components/Card.jsx";
 
 const App = () => {
-  return <div className="App">Hello world!</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jeans",
+    },
+    {
+      id: 3,
+      title: "Trousers",
+    },
+    {
+      id: 4,
+      title: "Men's",
+    },
+    {
+      id: 5,
+      title: "Women's",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <Card item={category.title} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
